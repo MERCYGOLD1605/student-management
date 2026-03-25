491c6ce90a47f25d1c289e0222ff4675cba691ad
Mercy <jangammercy97@gmail.com>
2026-03-25 17:28:07 +0530

    Student management system

---
+++ app.py
@@ -0,0 +1,71 @@
+def add_student():
+    name = input("Enter name: ")
+    age = input("Enter age: ")
+    course = input("Enter course: ")
+
+    with open("students.txt", "a") as file:
+        file.write(f"{name},{age},{course}\n")
+
+    print("Student added!")
+
+def view_students():
+    try:
+        with open("students.txt", "r") as file:
+            students = file.readlines()
+            if not students:
+                print("No records found!")
+            for i, s in enumerate(students, 1):
+                name, age, course = s.strip().split(",")
+                print(f"{i}. {name} | Age: {age} | Course: {course}")
+    except:
+        print("No file found!")
+
+def delete_student():
+    view_students()
+    try:
+        num = int(input("Enter number to delete: "))
+        with open("students.txt", "r") as file:
+            students = file.readlines()
+        students.pop(num - 1)
+        with open("students.txt", "w") as file:
+            file.writelines(students)
+        print("Deleted successfully!")
+    except:
+        print("Invalid input!")
+
+def update_student():
+    view_students()
+    try:
+        num = int(input("Enter number to update: "))
+        with open("students.txt", "r") as file:
+            students = file.readlines()
+
+        name = input("New name: ")
+        age = input("New age: ")
+        course = input("New course: ")
+
+        students[num - 1] = f"{name},{age},{course}\n"
+
+        with open("students.txt", "w") as file:
+            file.writelines(students)
+
+        print("Updated successfully!")
+    except:
+        print("Error updating!")
+
+while True:
+    print("\n1.Add 2.View 3.Update 4.Delete 5.Exit")
+    ch = input("Choose: ")
+
+    if ch == "1":
+        add_student()
+    elif ch == "2":
+        view_students()
+    elif ch == "3":
+        update_student()
+    elif ch == "4":
+        delete_student()
+    elif ch == "5":
+        break
+    else:
+        print("Invalid choice")
